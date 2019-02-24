0bb2f945c8b7422f31d9c1d3afce9583abb8349f
andreimelo <meloandrei69@gmail.com>
2019-02-24 22:38:17 +0800

    AM #comment first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Index from '../src/pages/Index'
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
